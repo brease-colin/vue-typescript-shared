dc21a4f6fde3e67b736648c25fb702ef36a885c0
Colin Bosman <colin@brease.nl>
2020-07-31 20:58:45 +0200

    Added component tests

---
+++ shared1/components/Header1.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>Header1: <slot /> {{ testComputed }}</h1>
+</template>
+
+<script lang=ts>
+import { defineComponent, computed } from '@vue/composition-api';
+
+export default defineComponent({
+    setup()
+    {
+        const testComputed = computed(() => ' computed');
+
+        return {
+            testComputed,
+        };
+    }
+});
+</script>
+
+<style>
+
+</style>
