3f6c18fdd6a3432647cc9019d553150385921e1a
Colin Bosman <colin@brease.nl>
2020-08-01 15:41:49 +0200

    Add module resolution in comments

--- project1/vue.config.js
+++ project1/vue.config.js
@@ -4,12 +4,22 @@ module.exports = {
   lintOnSave: false,
   configureWebpack: {
     resolve: {
-        alias: {
-          '@s1': path.resolve(__dirname, '../shared1'),
-          '@s2': path.resolve(__dirname, '../shared2'),
-          '@s3': path.resolve(__dirname, 'shared3'),
-        },
+      alias: {
+        '@s1': path.resolve(__dirname, '../shared1'),
+        '@s2': path.resolve(__dirname, '../shared2'),
+        '@s3': path.resolve(__dirname, 'shared3'),
+      },
+      // modules: [
+      //   path.resolve(__dirname, 'node_modules'),
+      //   'node_modules',
+      // ],
     },
+    // resolveLoader: {
+    //   modules: [
+    //     path.resolve(__dirname, 'node_modules'),
+    //     'node_modules',
+    //   ],
+    // },
     devtool: 'source-map',
 },
 }
